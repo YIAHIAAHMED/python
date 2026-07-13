d8a26518c9ea20dfd402963e8bc1a7c07a775303
YIAHIAAHMED <yiahiaahmed@gmail.com>
2026-07-13 23:48:42 +0600

    first commit

---
+++ module-01/first.py
@@ -0,0 +1,13 @@
+age = 45
+interest_rate = 2.5
+name = 'Jume e jo pathan'
+district = 'B Baria'
+# is_single = True
+# is_sleeping = False
+# print (age + interest_rate)
+# print(district)
+"""for multiline comment alt+shift+a (shift+option+a) """
+
+print(type(age))
+print('yiahia' + ' ' + 'ahmed')
+      
---
+++ module-01/input.py
@@ -0,0 +1,5 @@
+print('Now i need money')
+money = input('Give me some money: ')
+print('here is your money', money)
+
+# by default the input from user will be string type
