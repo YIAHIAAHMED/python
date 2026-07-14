128a2f60a1ca986d1bf270cf94826d0ba672f134
YIAHIAAHMED <yiahiaahmed@gmail.com>
2026-07-14 09:42:38 +0600

    condition

---
+++ model-01/condition.py
@@ -0,0 +1,14 @@
+# in, not, not in, is , is not
+
+a = 2 
+if a>5:
+    print( '5 er besi')
+    print( ' koto kisu')
+elif a>3:
+    print('olpo boro')
+elif a==2:
+    print ('ekdom dui er soman')
+else:
+    print('ckhotot choto')    
+
+# nested conditions
---
+++ model-01/operator.py
@@ -0,0 +1,16 @@
+# +,-,*,/,%,//
+num1 = 45
+num2 = 5
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(10 % 3) #reminder vag ses
+print(10//3) # Purno vag fol
+
+# no ++ and --
+
+num4 = 12
+num3 = 5
+power = num4**num3
+print(power)
