087acfeecf7fedbf5b96fdbcdd5961df8139007e
YIAHIAAHMED <yiahiaahmed@gmail.com>
2026-07-15 12:01:38 +0600

    for

---
+++ model-01/for.py
@@ -0,0 +1,28 @@
+# numbers = [5, 10, 15, 20, 25]
+# sum = 0
+# for num in numbers:
+#     # print(num)
+#     sum = sum + num
+# print(sum)
+
+# numbers = [5, 10, 15, 20, 25]
+# sum = 0
+# for num in numbers:
+#     print(num)
+#     sum = sum + num
+#     if sum > 20:
+#         print('bigger sum', sum)
+# print(sum)
+
+# text = 'Pagla Howar'
+# for char in text:
+#     print(char)
+
+# print(range(1,10))
+
+for i in range(1, 10, 3):
+    print(i)
+
+friends = ['nobel', 'ashir', 'rabi']
+for friend in friends:
+    print(index, friend)
