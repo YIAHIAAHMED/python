c0c23990b820c65a2280f350d86bde97f57e9751
YIAHIAAHMED <yiahiaahmed@gmail.com>
2026-07-29 07:25:58 +0600

    food

--- module-09/Restaurant-management.py
+++ module-09/Restaurant-management.py
@@ -25,7 +25,66 @@ class Employee(User):
 class Admin(User):
     def __init__(self, name, phone, email, address):
             super().__init__(name, phone, email, address)
-            self.employee = [] # eta hocche amader database
+            
 
-    def add_employee(self, name, email, phone, address):
-         employee = Employee(name, email, phone, address)
+    def add_employee(self, restaurant, empolee):
+         restaurant.add_employee(empolee)
+
+    def view_employee(self, restaurant):
+         restaurant.view_employee()
+        
+
+class Restaurant:
+     def __init__(self, name):
+          self.name = name
+          self.employees = [] # eta hocche amader database
+
+     def add_employee(self, employee):
+         self.employees.append(employee)
+
+     def view_employee(self):
+         print('Employee List!!')
+         for emp in self.employees:
+              print(emp.name, emp.email, emp.phone, emp.address)
+
+class Menu:
+    def __init__(self):
+          self.items = [] # items er database
+
+    def add_menu_item(self, item):
+         self.items.append(item)
+
+    def find_item(self, item_name):
+          for item in self.items:
+               if item.name.lower() == item_name.lower():
+                    return item
+          return None
+     
+    def remove_item(self, item_name):
+        item = self.find_item(item_name)
+        if item:
+            self.items.remove(item)
+            print("Item Deleted")
+        else:
+             print('Item not found') 
+
+    def show_menu(self):
+        print('******Menu*******')
+        print('Name\tPrice \t Quantity')     
+        for item in self.items:
+            print(f'{item.name}\t{item.price}\t{item.quantity}')
+               
+class FoodItem:
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+             
+                    
+
+
+mn = Menu()
+item = FoodItem('Pizza', 12, 10)
+mn.add_menu_item(item)
+mn.show_menu()
+
