d51db142e5b26ac713fdb4cb894762c711ecc9b1
YIAHIAAHMED <yiahiaahmed@gmail.com>
2026-07-29 09:00:08 +0600

    order

--- module-09/Restaurant-management.py
+++ module-09/Restaurant-management.py
@@ -12,6 +12,55 @@ class User(ABC):
         self.email = email
         self.address = address
 
+class Customer(User):
+    def __init__(self, name, phone, email, address):
+        super().__init__(name, phone, email, address)
+        self.cart = Order()
+
+    def view_menu(self, restaurant):
+        restaurant.menu.show_menu()
+
+    def add_to_cart(self, restaurant, item_name, quantity):
+        item = restaurant.menu.find_item(item_name)
+        if item:
+            item.quantity = quantity
+            self.cart.add_item(item)
+            print('Item added')
+        else:
+             print('Item not found')
+
+    def view_cart(self):
+         print('**View Cart**')
+         print('Name\tPirce\tQuantity')
+         for item, quantity in self.cart.items.items():
+              print(f'{item.name} {item.price} {item.quantity}')
+         print('Total Price:{self.cart.total_price}')
+
+class Order:
+    def __init__(self):
+         self.items = {}
+
+    def add_item(self, item):
+        if item in self.items:
+             self.items[item] += item.quantity # jodi item ta cart e already thake
+        else:
+             self.items[item] = item.quantity # jodi cart e na thake
+
+    def remove(self, item):
+         if item in self.items:
+              del self.items[item]
+
+    def total_price(self):
+         return sum(item.price * quantity for item, quantity in self.items.item())
+    
+    def clear(self):
+         self.items = {}
+         
+
+
+             
+
+
 class Employee(User):
     def __init__(self, name, phone, email, address, age, designation, salary):
         super().__init__(name, phone, email, address)
@@ -32,12 +81,19 @@ class Admin(User):
 
     def view_employee(self, restaurant):
          restaurant.view_employee()
+
+    def add_new_item(self, restaurant, item):
+        restaurant.menu.add_menu_item(item)
+
+    def remove_item(self, restaurant, item):
+        restaurant.menu.remove_item(item)
         
 
 class Restaurant:
      def __init__(self, name):
           self.name = name
           self.employees = [] # eta hocche amader database
+          self.menu = FoodItem()
 
      def add_employee(self, employee):
          self.employees.append(employee)
@@ -85,6 +141,8 @@ class FoodItem:
 
 mn = Menu()
 item = FoodItem('Pizza', 12, 10)
+item2 = FoodItem('Burger', 10, 30)
 mn.add_menu_item(item)
+mn.add_menu_item(item2)
 mn.show_menu()
 
