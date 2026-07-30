6c4c0852f4a9beadf35db243959ce501234caea8
YIAHIAAHMED <yiahiaahmed@gmail.com>
2026-07-30 09:15:07 +0600

    add

---
+++ anisul_islam/user_input.py
@@ -0,0 +1,42 @@
+# print('Student Information')
+# print('Name : Karim')
+# print('Age : 28')
+# print('GPA : 3.92')
+
+# name = 'Karim'
+# age = '28'
+# gpa = '3.75'
+
+# print('Student Information')
+# print('-'*30 )
+# print('Name : '+name)
+# print('Age : ' +age)
+# print('GPA : ' +gpa)
+
+
+# user theke input neya
+# name = input('Enter your name: ')
+# age = input('Enter your age: ')
+# gpa = input('Enter your gpa: ')
+
+# print('Student Information')
+# print('-'*30 )
+# print('Name : '+name)
+# print('Age : ' +age)
+# print('GPA : ' +gpa)
+
+
+# #best way add korar jonne input nibo
+# num1 = (input('Enter your first number: ')
+# num2 = input('Enter your first number: ')
+
+# result = int(num1) + int(num2)
+# print('the sum is:', result)
+
+# add korar jonne input nibo
+num1 = int(input('Enter your first number: '))
+num2 = int(input('Enter your second number: '))
+
+# result = (num1) + (num2)
+result = num1 - num2
+print('the sum is:', result)
