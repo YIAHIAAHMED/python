8a5e7fcdb1c1e61d47f82528b7a902fa1f3dd9d8
YIAHIAAHMED <yiahiaahmed@gmail.com>
2026-08-02 13:29:46 +0600

    set

---
+++ anisul_islam_2/set.py
@@ -0,0 +1,12 @@
+# set e item gulur kono order thake na
+# index diye value gulu access kora jabe na
+# kono duplicate value rakha jabe na
+# curly brakets or set function use kora jabe
+
+# num = {1, 2, 3, 4, 5, 5} # output 5 dekhabe na 2 bar
+# print(num)
+
+# Onno system
+num1 = {1, 2, 3, 4, 5 }
+num2 = set([4,5,6])
+print(num2)
---
+++ anisul_islam_2/tuple.py
@@ -0,0 +1,12 @@
+# tuple e first bracket use korte hoy
+students = (
+    ('Yiahia Ahmed', 27, 3, 56),
+    'Zakaria Ahmed',
+    'Ayan Ahmed'
+)
+# students [0] = 'Moynul islam' # erokom change kora jabe na 
+# 
+# kintu onek subidha, list er cheye onek fast kaj kore  
+print(students[0])
+print(students[1:])
+# print(students)
