acfe88eb700f82a6a98ca3b707e3492a32617bf1
YIAHIAAHMED <yiahiaahmed@gmail.com>
2026-08-03 14:48:49 +0600

    stack

--- anisul_islam_2/set.py
+++ anisul_islam_2/set.py
@@ -7,6 +7,21 @@
 # print(num)
 
 # Onno system
+# num1 = {1, 2, 3, 4, 5 }
+# num2 = set([4,5,6])
+# num2.add(7)
+# num2.remove(7)
+# # print(num2)
+# print(7 in num2) # 4 num2 er modde ache naki check kora
+
+# set er real uses
+
 num1 = {1, 2, 3, 4, 5 }
-num2 = set([4,5,6])
-print(num2)
+num2 = set([4,5,6,7])
+
+# print(num1 | num2)  # union set er sign | , eta dile num1 and num2 er sob man dekhabe
+
+# print(num1 & num2)  # intersection set er sign & , eta dile num1 and num2 er common man dekhabe
+
+
+print(num1 - num2)  # difference set er sign - , eta dile num1 and num2 er modde je gulu komon seta num1 theke bad diye num1 er baki gulur man dekhabe
---
+++ anisul_islam_2/stack_and_queue.py
@@ -0,0 +1,3 @@
+# stack 
+# push er mane stack e data rakha
+# pop er mane stack theke data rempve kora
