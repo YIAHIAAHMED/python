790381eb2b7f0fe8758c809c7c1b5059842b3717
YIAHIAAHMED <yiahiaahmed@gmail.com>
2026-08-04 06:33:41 +0600

    function

---
+++ anisul_islam_2/function.py
@@ -0,0 +1,46 @@
+# Function 2 prokar
+# 1. Library Function
+# 2. User Defined function
+
+# user defined function
+# kono 1 ti bar bar korle function use kora valo
+
+# 2 ti sonkhar jugfol
+# def add(x, y):
+#     sum = x + y
+#     print(sum)
+
+# add(10,20)
+# add(20,20)
+# add(30,20)
+# add(80,20)
+
+# 3 ti sonkhar jugfol
+
+# def addition(x, y,z):
+#     sum = x + y +z
+#     print(sum)
+
+# # add(10,20)
+# # add(20,20)
+# # add(30,20)
+# addition(80,20,10)
+
+
+# biugfol
+# def sub(x, y):
+#     sum = x - y
+#     print(sum)
+
+# # add(10,20)
+# # add(20,20)
+# # add(30,20)
+# sub(20,10)
+
+
+# without parameter
+def message():
+    print('No Parameter')
+
+
+message()
