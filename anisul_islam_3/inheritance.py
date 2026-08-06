7b4dc6b8517a258d264e6f9b83d36b7d3a703c9e
YIAHIAAHMED <yiahiaahmed@gmail.com>
2026-08-06 15:49:00 +0600

    meth

---
+++ anisul_islam_3/inheritance.py
@@ -0,0 +1,20 @@
+class Phone:
+    def call(self):
+        print('You can call')
+
+    def message(self):
+        print('You can message')
+
+class Samsung(Phone):
+
+
+    def photo(self):
+        print('You can take photo')
+
+# p = Phone()
+# p.message()
+# p.call()
+
+s = Samsung()
+s.message()
+s.call()
---
+++ anisul_islam_3/method_overriding.py
@@ -0,0 +1,9 @@
+class Phone:
+    def __init__(self):
+        print('I am in phone class')
+
+class Samsung(Phone):
+    def __init__(self):
+        super().__init__()
+
+s = Samsung()
